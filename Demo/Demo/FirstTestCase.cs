d0e706e31b0690c2bce8b3c4f6f6a8dc59f728d6
Tech-A28 <Tech-A28@Tech-A28s-MacBook-Air.local>
2016-12-21 17:30:23 +0000

    First commit on first attempt at C#

---
+++ Demo/Demo/FirstTestCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Demo
+{
+	class FirstTestCase
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new FirefoxDriver();
+			driver.Url = "http://www.demoqa.com";
+		}
+	}
+}
